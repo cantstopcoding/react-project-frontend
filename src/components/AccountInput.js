c5c4c615bc26280ec1286c87580cde3be3b400df
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-03 13:57:08 -0500

    set state in handleChange func, update input tags and create submit

--- src/components/AccountInput.js
+++ src/components/AccountInput.js
@@ -9,17 +9,23 @@ class AccountInput extends React.Component {
     };
 
     handleChange = (event) => {
-        // debugger
+        
+        this.setState({
+            [event.target.name]: event.target.value
+            // balance: event.target.value
+            
+        })
     }
 
     render() {
         return (
             <div>
                 <form>
-                    <label>Checking Account Name:</label><br/>
-                    <input type='text' placeholder='Name...'  value={this.state.name} name="name" onChange={this.handleChange}/><br/><br/>
-                    <label>Checking Account Balance:</label><br/>
+                    <label>Checking Account Name: </label>
+                    <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/><br/>
+                    <label>Checking Account Balance: </label>
                     <input type='text' placeholder='Balance' value={this.state.balance} name="balance" onChange={this.handleChange}/><br/>
+                    <input type='submit' />
                 </form>
             </div>
         )
