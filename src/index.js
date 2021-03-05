8314c8c349f54baf82791f0edf75cb75e5cf3618
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-05 08:13:55 -0500

    add app comp to router to set up links and routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import accountReducer from './reducers/accountReducer'
 import App from './App';
 
@@ -12,7 +13,9 @@ let store = createStore(accountReducer, composeEnhancers(applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
