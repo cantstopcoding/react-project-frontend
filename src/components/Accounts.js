4702adb2ce3a0524aa3699f73997329c397c9ee3
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-07 10:03:26 -0500

    add links to accounts

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom';
 import Account from './Account';
 
 const Accounts = (props) => {
     return (
         <div>
             {props.accounts.map(account => 
-                <div key={account.id}><Account account={account}/></div>
+                <li key={account.id}>
+                    <Link to={`/accounts/${account.id}`}>{account.name}</Link>
+                </li>
+                // <Account account={account}/>
             )}
         </div>
     )
