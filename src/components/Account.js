776ef677e89890748f86f2a29aa96ddedac28879
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-07 21:07:40 -0500

    add transaction container in account show page

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
+import TransactionsContainer from '../containers/TransactionsContainer';
 
 const Account = (props) => {
     let account = props.accounts[props.match.params.id - 1]
     console.log(account)
 
     return (
-
-        <h2>
-            {account ? account.name : null} - {account ? account.balance : null}
-        </h2>
+        <div>
+            <h2>
+                {account ? account.name : null} - {account ? account.balance : null}
+            </h2>
+            <TransactionsContainer account={account} />
+        </div>
     )
 }
 export default Account 
