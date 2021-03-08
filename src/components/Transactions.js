9934395a21535a1b540b857a90b6c50455bb2a7a
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-07 21:06:49 -0500

    create transactions component

---
+++ src/components/Transactions.js
@@ -0,0 +1,11 @@
+import React from 'react-redux';
+
+const Transactions = (props) => {
+    return (
+        <div>
+            Transactions 
+        </div>
+    )
+}
+
+export default Transactions
