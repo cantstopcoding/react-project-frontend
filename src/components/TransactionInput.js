68cc4f0d58c05cec8c7536e516cba3cf901c30f6
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-07 21:06:31 -0500

    create transaction input component

---
+++ src/components/TransactionInput.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TransactionInput extends Component {
+    render () {
+        return (
+            <div>
+                TransactionInput
+            </div>
+        )
+    }
+}
+
+export default TransactionInput
