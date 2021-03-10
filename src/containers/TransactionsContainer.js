15f7a49f7dd16d064e4107a05b96b618224efce8
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-10 18:33:35 -0500

    add account prop

--- src/containers/TransactionsContainer.js
+++ src/containers/TransactionsContainer.js
@@ -3,10 +3,11 @@ import TransactionInput from '../components/TransactionInput'
 import Transactions from '../components/Transactions'
 
 class TransactionsContainer extends Component {
+    
     render() {
         return (
             <div>
-                <TransactionInput/>
+                <TransactionInput account={this.props.account}/>
                 <Transactions transactions={this.props.account && this.props.account.transactions}/>
             </div>
         )
