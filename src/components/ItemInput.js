54a36beb96e07874468e7b62a21c7d56f9b20306
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-22 16:27:57 -0400

    create item input component

---
+++ src/components/ItemInput.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class ItemInput extends Component {
+    render() {
+        return (
+            <div>
+                ItemInput
+            </div>
+        )
+    }
+}
+
+export default ItemInput
