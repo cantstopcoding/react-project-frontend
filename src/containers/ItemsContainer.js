138905c5bc7fedb566b3b8ba4870ca0a417dfef1
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-22 15:47:23 -0400

    create items container component

---
+++ src/containers/ItemsContainer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class ItemsContainer extends Component {
+    render() {
+        <div>
+            AccountsContainer
+        </div>
+    }
+}
+
+export default ItemsContainer
