80f1a2908166889cd3c4df3c2b2f4252747c26db
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-28 12:27:04 -0400

    formatted

--- src/containers/AccountsContainer.js
+++ src/containers/AccountsContainer.js
@@ -1,34 +1,44 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import {Route, Switch} from 'react-router-dom'
-import {fetchAccounts} from '../actions/fetchAccounts'
-import AccountInput from '../components/AccountInput';
-import Accounts from '../components/Accounts';
-import Account from '../components/Account';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import { fetchAccounts } from "../actions/fetchAccounts";
+import AccountInput from "../components/AccountInput";
+import Accounts from "../components/Accounts";
+import Account from "../components/Account";
 
 class AccountsContainer extends Component {
-    componentDidMount() {
-        this.props.fetchAccounts()
-    }
+  componentDidMount() {
+    this.props.fetchAccounts();
+  }
 
-    render() {
-        console.log('ACCOUNT CONTAINER', this.props.accounts)
-        return (
-            <div>
-                <Switch>
-                    <Route path='/accounts/new' component={AccountInput} />
-                    <Route path='/accounts/:id' render={(routerProps) => <Account {...routerProps} accounts={this.props.accounts}/>} />
-                    <Route path='/accounts' render={(routerProps) => <Accounts {...routerProps} accounts={this.props.accounts}/>} />
-                </Switch>
-            </div>
-        )
-    }
+  render() {
+    console.log("ACCOUNT CONTAINER", this.props.accounts);
+    return (
+      <div>
+        <Switch>
+          <Route path="/accounts/new" component={AccountInput} />
+          <Route
+            path="/accounts/:id"
+            render={(routerProps) => (
+              <Account {...routerProps} accounts={this.props.accounts} />
+            )}
+          />
+          <Route
+            path="/accounts"
+            render={(routerProps) => (
+              <Accounts {...routerProps} accounts={this.props.accounts} />
+            )}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        accounts: state.accountReducer.accounts
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    accounts: state.accountReducer.accounts,
+  };
+};
 
-export default connect(mapStateToProps, {fetchAccounts})(AccountsContainer)
+export default connect(mapStateToProps, { fetchAccounts })(AccountsContainer);
