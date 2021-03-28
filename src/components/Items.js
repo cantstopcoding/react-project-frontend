e996752683c35a651c4d5ce70845062bcffd4e95
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-28 10:53:12 -0400

    import link, map items and make defaultProps

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,11 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 
 const Items = (props) => {
-    return (
-        <div>
-            Items 
-        </div>
-    )
-}
+  return (
+    <div>
+      {props.items.map((item) => (
+        <li key={item.id}>
+          <Link to={`/items/${item.id}`}>{item.name}</Link>
+        </li>
+      ))}
+    </div>
+  );
+};
 
-export default Items 
+Items.defaultProps = {
+  items: [],
+};
+
+export default Items;
+
+// return (
+//     <div>
+//         {props.accounts.map(account =>
+//             <li key={account.id}>
+//                 <Link to={`/accounts/${account.id}`}>{account.name}</Link>
+//             </li>
+//             // <Account account={account}/>
+//         )}
+//     </div>
