2ce2cae9c3add802c9a74d1e3ca536dd32be4a55
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-15 16:22:41 -0400

    delete account from account index page

---
+++ src/actions/deleteAccount.js
@@ -0,0 +1,12 @@
+export const deleteAccount = (accountId) => {
+  return (dispatch) => {
+    return fetch(`http://localhost:3000/api/v1/accounts/${accountId}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(
+        (accounts) => dispatch({ type: "DELETE_ACCOUNT", payload: accounts }) //accounts are array of accounts
+      )
+      .catch((err) => console.error("ERR", err));
+  };
+};
