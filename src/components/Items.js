eb22cbf26bf53c39d7bb0cf1031efc11fc2f8036
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-15 18:21:35 -0400

    put ItemEditOnAccount model in in Items.js

--- src/components/Items.js
+++ src/components/Items.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Route, Link } from "react-router-dom";
 import { deleteItem } from "../actions/deleteItem";
 import Item from "../components/Item";
+import ItemEditOnAccount from "./ItemEditOnAccount";
 
 const Items = (props) => {
   console.log("ITEM", props.items);
@@ -22,6 +23,7 @@ const Items = (props) => {
               <Link to={`/accounts/${item.account_id}/items/${item.id}`}>
                 {item.name} - Price: ${item.price} {item.description}
               </Link>
+              <ItemEditOnAccount />
               <button onClick={() => handleDelete(item)}>Delete</button>
             </li>
           ))}
