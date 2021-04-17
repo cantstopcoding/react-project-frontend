29792011fa72a4b2db1563d24765fd3581388c5d
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-17 09:12:43 -0400

    create form

--- src/components/ItemEditOnAccount.js
+++ src/components/ItemEditOnAccount.js
@@ -3,71 +3,77 @@ import { connect } from "react-redux";
 import { editItemOnAccount } from "../actions/editItemOnAccount";
 
 class ItemEditOnAccount extends React.Component {
-  //   state = {
-  //     first_name: "",
-  //     last_name: "",
-  //     username: "",
-  //     email: "",
-  //   };
-  //   handleChange = (event) => {
-  //     this.setState({
-  //       [event.target.name]: event.target.value,
-  //     });
-  //   };
-  //   handleSubmit = (event) => {
-  //     event.preventDefault();
-  //     let account = { ...this.state, id: this.props.account.id };
-  //     this.props.editAccount(account);
-  //     this.setState({
-  //       first_name: "",
-  //       last_name: "",
-  //       username: "",
-  //       email: "",
-  //     });
-  //   };
+  state = {
+    name: "",
+    image_url: "",
+    description: "",
+    price: "",
+  };
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // debugger;
+
+    // this.props.editItemOnAccount(account.id, item.id);
+    this.props.editItemOnAccount(
+      this.props.item.account_id,
+      this.props.item.id
+    );
+    this.setState({
+      name: "",
+      image_url: "",
+      description: "",
+      price: "",
+    });
+  };
+
   render() {
     return (
       <div>
-        Edit Item On Account
-        {/* <form onSubmit={this.handleSubmit}>
-            <label>First Name: </label>
-            <input
-              type="text"
-              placeholder="First Name"
-              value={this.state.first_name}
-              name="first_name"
-              onChange={this.handleChange}
-            />
-            <br />
-            <label>Last Name: </label>
-            <input
-              type="text"
-              placeholder="Last Name"
-              value={this.state.last_name}
-              name="last_name"
-              onChange={this.handleChange}
-            />
-            <br />
-            <label>Username: </label>
-            <input
-              type="text"
-              placeholder="Username"
-              value={this.state.username}
-              name="username"
-              onChange={this.handleChange}
-            />
-            <br />
-            <label>Email: </label>
-            <input
-              type="text"
-              placeholder="Email"
-              value={this.state.email}
-              name="email"
-              onChange={this.handleChange}
-            />
-            <br />
-            <input type="submit" />
-          </form> */}
+        <form onSubmit={this.handleSubmit}>
+          <h3>Edit Item:</h3>
+          <label>Name: </label>
+          <input
+            type="text"
+            placeholder="Name"
+            value={this.state.first_name}
+            name="name"
+            onChange={this.handleChange}
+          />
+          <br />
+          <label>Image Url: </label>
+          <input
+            type="text"
+            placeholder="Image Url"
+            value={this.state.last_name}
+            name="image_url"
+            onChange={this.handleChange}
+          />
+          <br />
+          <label>Description: </label>
+          <input
+            type="text"
+            placeholder="Description"
+            value={this.state.username}
+            name="description"
+            onChange={this.handleChange}
+          />
+          <br />
+          <label>Price: </label>
+          {/* <input
+            type="text"
+            placeholder="Price"
+            value={this.state.price}
+            name="price"
+            onChange={this.handleChange}
+          /> */}
+          <br />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
