36c737658b0859586eb98b239a4689797f845413
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-18 22:08:53 -0400

    create home, layouts and nomatch components

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Home = () => (
   <div>
-    <h2>Hi Everyone!</h2>
-    <p>Animals are the best!</p>
-    <p>Dogs, cats and everything in betweens</p>
+    <h2>Caroline DAmbrosio Designs!</h2>
+    <p>Go back in time and wear old timey dresses.</p>
+    <p>I make Victorian Dresses.</p>
   </div>
 );
--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,3 +1,7 @@
 import React from "react";
 
-export const NoMatch = () => <div></div>;
+export const NoMatch = () => (
+  <div>
+    <p>No Match</p>
+  </div>
+);
