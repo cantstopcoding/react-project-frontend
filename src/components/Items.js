1a8846df0e1f47d255d499a182ba8eb27c21b29d
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-20 15:15:39 -0400

    make cards horizontal

--- src/components/Items.js
+++ src/components/Items.js
@@ -4,7 +4,7 @@ import { Route, Link } from "react-router-dom";
 import { deleteItem } from "../actions/deleteItem";
 import Item from "../components/Item";
 import ItemEditOnAccount from "./ItemEditOnAccount";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, CardDeck } from "react-bootstrap";
 
 const Items = (props) => {
   console.log("ITEM", props.items);
@@ -18,36 +18,26 @@ const Items = (props) => {
     <div>
       <h2>My Items:</h2>
       <div>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-
-        {props.items &&
-          props.items.map((item) => (
-            <div key={item.id}>
-              <Card style={{ width: "30rem" }}>
-                <Card.Img variant="top" src={item.image_url} />
-                <Card.Body>
-                  <Card.Title>{item.name}</Card.Title>
-                  <Card.Text>
-                    Price: ${item.price}
-                    <br />
-                    Description: {item.description}
-                    <ItemEditOnAccount item={item} />
-                    <button onClick={() => handleDelete(item)}>Delete</button>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-          ))}
+        <CardDeck style={{ display: "flex", flexDirection: "row" }}>
+          {props.items &&
+            props.items.map((item) => (
+              <div key={item.id}>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={item.image_url} />
+                  <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Text>
+                      Price: ${item.price}
+                      <br />
+                      Description: {item.description}
+                      <ItemEditOnAccount item={item} />
+                      <button onClick={() => handleDelete(item)}>Delete</button>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
+        </CardDeck>
       </div>
     </div>
   );
