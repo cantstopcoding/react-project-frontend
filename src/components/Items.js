c88a66513e2d45f95df06e897fd8979b7652c284
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-20 15:03:44 -0400

    make items into cards

--- src/components/Items.js
+++ src/components/Items.js
@@ -4,6 +4,7 @@ import { Route, Link } from "react-router-dom";
 import { deleteItem } from "../actions/deleteItem";
 import Item from "../components/Item";
 import ItemEditOnAccount from "./ItemEditOnAccount";
+import { Card, Button } from "react-bootstrap";
 
 const Items = (props) => {
   console.log("ITEM", props.items);
@@ -17,21 +18,36 @@ const Items = (props) => {
     <div>
       <h2>My Items:</h2>
       <div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
         {props.items &&
           props.items.map((item) => (
-            <li key={item.id}>
-              <Link to={`/accounts/${item.account_id}/items/${item.id}`}>
-                {item.name} - Price: ${item.price} {item.description}
-              </Link>
-              {/* pass props in component below */}
-              {/* which one?  */}
-              <ItemEditOnAccount item={item} />
-              <button onClick={() => handleDelete(item)}>Delete</button>
-              <br />
-              <br />
-            </li>
+            <div key={item.id}>
+              <Card style={{ width: "30rem" }}>
+                <Card.Img variant="top" src={item.image_url} />
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                  <Card.Text>
+                    Price: ${item.price}
+                    <br />
+                    Description: {item.description}
+                    <ItemEditOnAccount item={item} />
+                    <button onClick={() => handleDelete(item)}>Delete</button>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
           ))}
-        <br />
       </div>
     </div>
   );
