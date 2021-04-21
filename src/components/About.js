2cdcb9afd0a03d0773c076897981db19050e67e0
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-21 17:56:03 -0400

    change abount page info

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,15 @@ import React from "react";
 export const About = () => (
   <div>
     <h2>About Page</h2>
-    <p>Go back in time and wear old timey dresses.</p>
-    <p>I make Victorian Dresses.</p>
+    <p>
+      Hello! Thank you so much for coming to my website! I love to create
+      historical fashion and vintage fashion. I am a self taught seamstress for
+      about 2 years now. I would love to build a community of like minded people
+      to explore creating costumes!
+    </p>
+    <p>
+      Please consider supporting me on patreon for more content and following me
+      on instagram!
+    </p>
   </div>
 );
