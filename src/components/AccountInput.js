c6e2e826dfe5de899d5f91b6e8410c09d2b0e469
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-24 21:31:11 -0400

    abstract edit and create account forms

--- src/components/AccountInput.js
+++ src/components/AccountInput.js
@@ -1,77 +1,12 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { addAccount } from "../actions/addAccount";
+import React from "react";
+import AccountForm from "./AccountForm";
 
-class AccountInput extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    username: "",
-    email: "",
-  };
+const AccountInput = () => {
+  return (
+    <div>
+      <AccountForm />
+    </div>
+  );
+};
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.addAccount(this.state);
-    this.setState({
-      firstName: "",
-      lastName: "",
-      username: "",
-      email: "",
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>First Name: </label>
-          <input
-            type="text"
-            placeholder="First Name"
-            value={this.state.firstName}
-            name="firstName"
-            onChange={this.handleChange}
-          />
-          <br />
-          <label>Last Name: </label>
-          <input
-            type="text"
-            placeholder="Last Name"
-            value={this.state.lastName}
-            name="lastName"
-            onChange={this.handleChange}
-          />
-          <br />
-          <label>Username: </label>
-          <input
-            type="text"
-            placeholder="Username"
-            value={this.state.username}
-            name="username"
-            onChange={this.handleChange}
-          />
-          <br />
-          <label>Email: </label>
-          <input
-            type="text"
-            placeholder="Email"
-            value={this.state.email}
-            name="email"
-            onChange={this.handleChange}
-          />
-          <br />
-          <input type="submit" />
-        </form>
-      </div>
-    );
-  }
-}
-
-export default connect(null, { addAccount })(AccountInput);
+export default AccountInput;
