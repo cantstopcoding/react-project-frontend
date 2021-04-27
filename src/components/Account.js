46a5009a3520a4a1e990531ccaf9a891ff3dd65d
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-27 08:32:22 -0400

    display NoMatch route if account show url does not exist

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,10 +1,11 @@
 import React from "react";
 import AccountEdit from "./AccountEdit";
+import { Route } from "react-router-dom";
+import NoMatch from "../components/NoMatch";
 
 import ItemsContainer from "../containers/ItemsContainer";
 
 const Account = (props) => {
-  // let account = props.accounts[props.match.params.id - 1]
   let account = props.accounts.filter(
     // match.params.id is a string, not a number!!!
     (account) => `${account.id}` == props.match.params.id
@@ -13,12 +14,13 @@ const Account = (props) => {
   return (
     <div>
       <h2>
-        {account ? account.firstName : null} {account ? account.lastName : null}{" "}
-        - {account ? account.username : null}
+        {account ? account.firstName : <Route component={NoMatch} />}{" "}
+        {account ? account.lastName : null} -{" "}
+        {account ? account.username : null}
       </h2>
-      <AccountEdit account={account} />
+      {account ? <AccountEdit account={account} /> : null}
       <br />
-      <ItemsContainer account={account} />
+      {account ? <ItemsContainer account={account} /> : null}
     </div>
   );
 };
