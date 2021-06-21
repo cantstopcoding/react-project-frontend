0b1664b32bf27b8de77ce830bdf7605726e033c0
cantstopcoding <jonathan_koss@yahoo.com>
2021-06-21 07:35:12 -0400

    move components out of accounts container

--- src/actions/fetchAccounts.js
+++ src/actions/fetchAccounts.js
@@ -2,12 +2,12 @@ export function fetchAccounts() {
   return (dispatch) => {
     fetch("http://localhost:3000/api/v1/accounts")
       .then((response) => response.json())
-      .then((accounts) =>
+      .then((accounts) => {
         dispatch({
           type: "FETCH_ACCOUNTS",
           payload: accounts,
-        })
-      )
+        });
+      })
       .catch((err) => console.error("ERR", err));
   };
 }
--- src/containers/AccountsContainer.js
+++ src/containers/AccountsContainer.js
@@ -5,10 +5,7 @@ import { fetchAccounts } from "../actions/fetchAccounts";
 import AccountInput from "../components/AccountInput";
 import Accounts from "../components/Accounts";
 import Account from "../components/Account";
-import Home from "../components/Home";
-import About from "../components/About";
-import Contact from "../components/Contact";
-import NoMatch from "../components/NoMatch";
+
 import Layout from "../components/Layouts";
 
 class AccountsContainer extends Component {
@@ -22,9 +19,6 @@ class AccountsContainer extends Component {
         <React.Fragment>
           <Layout>
             <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/contact" component={Contact} />
               <Route exact path="/accounts/new" component={AccountInput} />
               <Route
                 exact
@@ -40,7 +34,7 @@ class AccountsContainer extends Component {
                   <Accounts {...routerProps} accounts={this.props.accounts} />
                 )}
               />
-              <Route component={NoMatch} />
+              {/* <Route component={NoMatch} /> */}
             </Switch>
           </Layout>
         </React.Fragment>
