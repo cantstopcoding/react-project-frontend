bd39c16441eaf40c5fc4782057a463196c0c2bf7
cantstopcoding <jonathan_koss@yahoo.com>
2021-06-23 14:36:18 -0400

    make items container a presentational compnent

--- src/containers/ItemsContainer.js
+++ src/containers/ItemsContainer.js
@@ -1,36 +1,16 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Switch, Route } from "react-router";
 import ItemInput from "../components/ItemInput";
 import Items from "../components/Items";
-import { fetchItem } from "../actions/fetchItem";
 
-class ItemsContainer extends Component {
-  componentDidMount() {
-    this.props.fetchItem(1, 1);
-  }
-
-  render() {
-    return (
+const ItemsContainer = (props) => {
+  return (
+    <div>
       <div>
-        <Switch>
-          <Route exact path="/accounts/:id">
-            <div>
-              <ItemInput account={this.props.account} />
-              <Items items={this.props.account && this.props.account.items} />
-            </div>
-          </Route>
-        </Switch>
+        <ItemInput account={props.account} />
+        <Items items={props.account && props.account.items} />
       </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  console.log(state.accountReducer);
-  return {
-    item: state.accountReducer.items,
-  };
+    </div>
+  );
 };
 
-export default connect(mapStateToProps, { fetchItem })(ItemsContainer);
+export default ItemsContainer;
