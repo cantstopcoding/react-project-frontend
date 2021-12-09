de825899575c5b4686112194c3c048ba84eea89b
cantstopcoding <jonathan_koss@yahoo.com>
2021-12-09 12:41:35 -0500

    turn Accounts components into a functional one

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -4,23 +4,21 @@ import { Link } from "react-router-dom";
 
 import { deleteAccount } from "../actions/deleteAccount";
 
-class Accounts extends React.Component {
-  handleDelete = (account) => {
-    this.props.deleteAccount(account.id);
-  };
-
-  render() {
-    return (
-      <div>
-        {this.props.accounts.map((account) => (
-          <li key={account.id}>
-            <Link to={`/accounts/${account.id}`}>{account.firstName}</Link>
-            <button onClick={() => this.handleDelete(account)}>Delete</button>
-          </li>
-        ))}
-      </div>
-    );
+const Accounts = (props) => {
+  function handleDelete(account) {
+    props.deleteAccount(account.id);
   }
-}
+
+  return (
+    <div>
+      {props.accounts.map((account) => (
+        <li key={account.id}>
+          <Link to={`/accounts/${account.id}`}>{account.firstName}</Link>
+          <button onClick={() => handleDelete(account)}>Delete</button>
+        </li>
+      ))}
+    </div>
+  );
+};
 
 export default connect(null, { deleteAccount })(Accounts);
