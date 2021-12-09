88a74486f9428f399199f6ff629353287087508e
cantstopcoding <jonathan_koss@yahoo.com>
2021-12-08 23:04:05 -0500

    turn items component into a functional component

--- src/components/Items.js
+++ src/components/Items.js
@@ -5,86 +5,39 @@ import ItemEditOnAccount from "./ItemEditOnAccount";
 import { Card, CardDeck } from "react-bootstrap";
 import LikeButton from "./LikeButton";
 
-class Items extends React.Component {
-  state = {
-    number: "",
-  };
-
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  handleDelete = (item) => {
-    this.props.deleteItem(item.id, item.accountId);
-  };
-
-  // list to when button is clicked
-  // input for the number that was enter
-  // add number to the button
-  // if all numbers are incrementing, you might want to make a new component
-
-  // make a new component
-  // import the value of that input into the this.props and add the the value of that to incrementNumber
-  incrementNumber = (event, state) => {
-    console.log(event.target.innerText);
-    const html = parseInt(event.target.innerText);
-    const stateNumber = parseInt(state.number);
-
-    if (isNaN(stateNumber)) {
-      return event.target.innerText;
-    } else {
-      return (event.target.innerText = html + stateNumber);
-    }
-  };
-
-  render() {
-    return (
-      <div>
-        <div>
-          Increment By:
-          <input
-            value={this.state.number}
-            name="number"
-            onChange={this.handleChange}
-          />
-        </div>
-
-        <h2>My Items:</h2>
-
-        <div>
-          <CardDeck style={{ display: "flex", flexDirection: "row" }}>
-            {this.props.items &&
-              this.props.items.map((item) => (
-                <div key={item.id}>
-                  <Card style={{ width: "21.5rem", height: "97.5%" }}>
-                    <Card.Img variant="top" src={item.imageUrl} />
-                    <Card.Body>
-                      <Card.Title>{item.name}</Card.Title>
-                      Price: ${item.price}
-                      <br />
-                      Description: {item.description}
-                      <br />
-                      <button onClick={() => this.handleDelete(item)}>
-                        Delete
-                      </button>
-                      <button
-                        onClick={(e) => this.incrementNumber(e, this.state)}
-                      >
-                        0
-                      </button>
-                      <LikeButton />
-                      <ItemEditOnAccount item={item} />
-                    </Card.Body>
-                  </Card>
-                </div>
-              ))}
-          </CardDeck>
-        </div>
-      </div>
-    );
+const Items = (props) => {
+  function handleDelete(item) {
+    props.deleteItem(item.id, item.accountId);
   }
-}
+
+  return (
+    <>
+      <h2>My Items:</h2>
+
+      <div>
+        <CardDeck style={{ display: "flex", flexDirection: "row" }}>
+          {props.items &&
+            props.items.map((item) => (
+              <div key={item.id}>
+                <Card style={{ width: "21.5rem", height: "97.5%" }}>
+                  <Card.Img variant="top" src={item.imageUrl} />
+                  <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                    Price: ${item.price}
+                    <br />
+                    Description: {item.description}
+                    <br />
+                    <button onClick={() => handleDelete(item)}>Delete</button>
+                    {<LikeButton />}
+                    <ItemEditOnAccount item={item} />
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
+        </CardDeck>
+      </div>
+    </>
+  );
+};
 
 export default connect(null, { deleteItem })(Items);
