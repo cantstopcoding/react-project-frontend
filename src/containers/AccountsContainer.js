48a7d37744b94ee0da9abb8ce010f8aee47e4040
cantstopcoding <jonathan_koss@yahoo.com>
2021-12-09 22:59:57 -0500

    in AccountsContainer convert to useEffect

--- src/containers/AccountsContainer.js
+++ src/containers/AccountsContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { fetchAccounts } from "../actions/fetchAccounts";
@@ -8,37 +8,35 @@ import Account from "../components/Account";
 
 import Layout from "../components/Layouts";
 
-class AccountsContainer extends Component {
-  componentDidMount() {
-    this.props.fetchAccounts();
-  }
+const AccountsContainer = (props) => {
+  useEffect(() => {
+    props.fetchAccounts();
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route exact path="/accounts/new" component={AccountInput} />
-            <Route
-              exact
-              path="/accounts/:id"
-              render={(routerProps) => (
-                <Account {...routerProps} accounts={this.props.accounts} />
-              )}
-            />
-            <Route
-              exact
-              path="/accounts"
-              render={(routerProps) => (
-                <Accounts {...routerProps} accounts={this.props.accounts} />
-              )}
-            />
-          </Switch>
-        </Layout>
-      </div>
-    );
-  }
-}
+  return (
+    <>
+      <Layout>
+        <Switch>
+          <Route exact path="/accounts/new" component={AccountInput} />
+          <Route
+            exact
+            path="/accounts/:id"
+            render={(routerProps) => (
+              <Account {...routerProps} accounts={props.accounts} />
+            )}
+          />
+          <Route
+            exact
+            path="/accounts"
+            render={(routerProps) => (
+              <Accounts {...routerProps} accounts={props.accounts} />
+            )}
+          />
+        </Switch>
+      </Layout>
+    </>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
